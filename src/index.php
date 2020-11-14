b6ee96693b18afcb2dc0efcf1479f88527d2fa98
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-11-14 17:01:36 +0100

    added page component locations for index.php

--- src/index.php
+++ src/index.php
@@ -1,12 +1,20 @@
+<?php require_once 'protected/config.php'; ?>
+
 <!DOCTYPE html>
 <html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+
 </head>
 <body>
+    <div class = "container">
+        <header><?php include_once PROTECTED_DIR.'header.php' ?></header>
+        <nav><?php require_once PROTECTED_DIR.'nav.php' ?></nav>
+        <content><?php require_once PROTECTED_DIR.'routing.php' ?></content>
+        <footer><?php include_once PROTECTED_DIR.'footer.php' ?></footer>
+    </div>
     
 </body>
 </html>
