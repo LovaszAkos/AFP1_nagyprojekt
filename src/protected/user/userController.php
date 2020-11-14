8c32709ec250f1e6f78a572f41a400e8eeaf9ab4
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-11-14 17:21:27 +0100

    Added userLogout function to userController

--- src/protected/user/userController.php
+++ src/protected/user/userController.php
@@ -3,6 +3,10 @@ function IsUserLoggedIn() {
 	return $_SESSION  != null && array_key_exists('uid', $_SESSION) && is_numeric($_SESSION['uid']);
 }
 
-
+function UserLogout() { 
+	session_unset();
+	session_destroy();
+	header('Location: index.php');
+}
 
 ?>
