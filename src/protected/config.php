04a2c0ae492c12145aaa39887dd9df584060268d
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-11-14 16:58:05 +0100

    Expanded config.php file with directions and database information

---
+++ src/protected/config.php
@@ -0,0 +1,13 @@
+<?php
+    define('BASE_DIR', './');
+    define('PROTECTED_DIR', BASE_DIR.'protected/');
+    define('PUBLIC_DIR', BASE_DIR.'public/');
+    define('DATABASE_CONTROLLER', PROTECTED_DIR.'database.php');
+
+    define('DB_TYPE', 'mysql');
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', '');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_CHARSET', 'utf8');
+?>
