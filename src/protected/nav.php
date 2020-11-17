d7c6334d434bef65775ef725dcce3a0469ce5df1
Fügedi Csaba <fugedic@gmail.com>
2020-11-17 11:46:11 +0100

    Added nav.php

--- src/protected/nav.php
+++ src/protected/nav.php
@@ -1,1 +1,18 @@
-nav
+<hr>
+<div class ="row justify-content-md-center">
+    <div class = "col align-self-start">
+        <div class ="menuitem"><a href="index.php">Főoldal</a></div>
+        <?php if(isUserLoggedIn() && $_SESSION['permission'] >= 1) : ?>
+            <div class ="menuitem"><a href = "index.php?P=addProduct">Termék hozzáadás</a></div>
+        <?php endif; ?>
+    </div>
+
+    <div class = "align-self-center">
+        <div class = "menuitem"><a href = "index.php?P=listProducts&category=Gitár">Gitárok</a></div>
+        <div class = "menuitem"><a href = "index.php?P=listProducts&category=Effekt">Effektek</a></div>
+        <div class = "menuitem"><a href = "index.php?P=listProducts&category=Erősítő">Erősítők</a></div>
+        <div class = "menuitem"><a href = "index.php?P=listProducts&category=Kiegészítő">Kiegészítők</a></div>
+        
+    </div>
+
+   
