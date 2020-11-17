ae67fa6018fe94c4e9d2938061b4185e802c8264
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-11-17 12:22:03 +0100

    changed userController location

--- src/protected/user/userController.php
+++ src/protected/user/userController.php
@@ -29,5 +29,30 @@ function UserLogin($email, $password) {
 	return false;
 }
 
+function UserRegister($email, $password, $first_name, $last_name, $address, $zip, $city, $country) {
+    $query = "SELECT id FROM users email = :email";
+    $params = [ ':email' => $email ];
+
+    require_once DATABASE_CONTROLLER;
+    $record = getRecord($query, $params);
+    if(empty($record)) {
+        $query = "INSERT INTO users (first_name, last_name, email, password, address, zip, city, country) VALUES (:first_name, :last_name, :email, :password, :address, :zip, :city, :country)";
+        $params = [
+            ':first_name' => $first_name,
+            ':last_name' => $last_name,
+            ':email' => $email,
+            ':password' => sha1($password),
+            ':address' => $address,
+            ':zip' => $zip,
+            ':city' => $city,
+            ':country' => $country
+        ];
+
+        if(executeDML($query, $params))
+            header('Location: index.php?P=login');
+    }
+    return false;
+}
+
 
 ?>
