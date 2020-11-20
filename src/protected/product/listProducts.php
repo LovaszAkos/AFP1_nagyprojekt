e0250bddc69b9ad4d321338e5637c462200e678a
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-11-20 15:55:20 +0100

    Added product listing page frontend

--- src/protected/product/listProducts.php
+++ src/protected/product/listProducts.php
@@ -3,3 +3,24 @@
     $category = $_GET['category'];
     $items = getProductsByCategory($category);
 ?>
+
+<h2><?=$category ?></h2>
+
+<?php if(count($items) == 0) : ?>
+    <p id="alert">Nincs elem ebben a kategóriában!</p>
+<?php else: ?>
+    <div class = "row">
+        <?php foreach ($items as $item): ?>
+            <div class = "col">
+                <div class="card border-white" style="width: 15rem;">
+                    <a href="index.php?P=product&ID=<?=$item['id'] ?>"><div class="prwimg"><img src="<?=IMG_DIR.$item['picture']; ?>" class="card-img-top" alt="..."></div></a>
+                    <div class="card-body">
+                        <h5 class="card-title"><?=$item['product_brand'].' '.$item['product_name'] ?></h5>
+                        <h6 class="card-subtitle text-muted"><?=number_format($item['price']).' Ft'?></h6>
+                        <p class="card-text"><?=$item['shortdesc']; ?></p>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach ?>
+    </div>
+<?php endif; ?>
