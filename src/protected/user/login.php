3ef5f8b13d88dfe586c51fdcc2107a2915dc9d77
Tóbi Csanádi <csanadi.tobias@gmail.com>
2020-11-20 15:59:37 +0100

    Added frontend for login

--- src/protected/user/login.php
+++ src/protected/user/login.php
@@ -16,3 +16,30 @@
       }
   }
 ?>
+
+<h2>Bejelentkezés</h2>
+<form method="post">
+  <div class="form-row justify-content-md-center">
+    <div class="form-group col-md-4">
+      <label for="loginEmail">Email cím</label>
+      <input type="email" class="form-control" id="loginEmail" aria-describedby="emailHelp" name="email" value="<?= isset($postData) ? $postData['email'] : '';?>" required>
+      <small id="emailHelp" class="form-text text-muted"></small>
+    </div>
+  </div>
+  <div class="form-row justify-content-md-center">
+    <div class="form-group col-md-4">
+      <label for="loginPassword">Jelszó</label>
+      <input type="password" class="form-control" id="loginPassword" name="password" value="" required>
+    </div>
+  </div>
+  <div class="row justify-content-md-center">
+    <div class="col-md-auto">
+      <button type="submit" class="btn btn-primary" name="login">Login</button>
+    </div>
+  </div>
+  <div class="row justify-content-md-center">
+    <div class="col-md-auto">
+      <a href="<?='index.php?P=forgottenPassword' ?>" >Elfelejtett jelszó</a>
+    </div>
+  </div>
+</form>
