84d7af15e51ffe40bce5022c2c2d9b848dd68ed3
Fügedi Csaba <fugedic@gmail.com>
2020-11-22 10:47:58 +0100

    Edited config file NOTE: Later will change

--- src/protected/config.php
+++ src/protected/config.php
@@ -15,7 +15,7 @@
 
     define('DB_TYPE', 'mysql');
     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'audio');
+    define('DB_NAME', 'hangszerbolt');
     define('DB_USER', 'root');
     define('DB_PASS', '');
     define('DB_CHARSET', 'utf8');
