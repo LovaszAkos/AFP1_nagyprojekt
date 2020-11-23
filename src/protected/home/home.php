cf2b995f9e6630f75f510e8bbbd4da6f63fd3cdf
Nagy Zoltan <zolya1999@gmail.com>
2020-11-23 18:16:33 +0100

    Added base structure of the carousel

---
+++ src/protected/home/home.php
@@ -0,0 +1,44 @@
+<div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <a href = "index.php?P=listProducts&category=Git%C3%A1r">
+                <img src=# class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>-</h5>
+                    <p>-</p>
+                </div>
+            </a>
+        </div>
+        <div class="carousel-item">
+            <a href = "index.php?P=product&ID=10">
+                <img src=# class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>-</h5>
+                    <p>-</p>
+                </div>
+            </a>
+        </div>
+        <div class="carousel-item">
+            <a href = "index.php?P=product&ID=9">
+                <img src=# class="d-block w-100" alt="...">
+                <div class="carousel-caption d-none d-md-block">
+                    <h5>-</h5>
+                    <p>-</p>
+                </div>
+            </a>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
