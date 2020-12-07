a51d244dea2a6690adc6b667b358c19e7dff2bc1
Fügedi Csaba <fugedic@gmail.com>
2020-12-07 19:22:30 +0100

    Updated Forgotten password page

--- src/protected/user/forgottenPsw.php
+++ src/protected/user/forgottenPsw.php
@@ -1,1 +1,3 @@
-<p>Elfelejtett jelszó odal</p>
+<p align="center">
+<iframe width="1000" height="685" src="https://www.youtube.com/embed/dQw4w9WgXcQ?rel=0&amp&autoplay=1&mute=1" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</p>
